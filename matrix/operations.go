6697fb47114679dbccdcc4374ed2be640f7ee61d
Seon-Wook Park <seon.wook@swook.net>
2012-12-12 02:19:31 +0000

    Make Add in-place op. Allow int,float64 params

--- matrix/operations.go
+++ matrix/operations.go
@@ -4,33 +4,49 @@ import (
 	"fmt"
 )
 
-type operator func(int, int, float64) float64
+type operator func(int, int) error
 
 func (m Matrix) Foreach(op operator) {
 	// Iterate over rows
 	for y, vy := range m {
 		// Iterate over columns
-		for x, vx := range vy {
+		for x, _ := range vy {
 			// Apply operation to matrix element
-			op(y, x, vx)
+			op(y, x)
 		}
 	}
 	return
 }
 
-func (A Matrix) Add(B Matrix) (C Matrix, err error) {
-	sA := A.Size()
-	sB := B.Size()
-	if (sA.Y != sB.Y) || (sA.X != sB.X) {
-		err = fmt.Errorf("go.iccp/matrix: Matrix dimensions mis-match.")
-		return
-	}
-
-	C, _ = New(sA)
-	for y := 0; y < sA.Y; y++ {
-		for x := 0; x < sA.X; x++ {
-			C[y][x] = A[y][x] + B[y][x]
+func (A Matrix) Add(in ...interface{}) (err error) {
+	switch in[0].(type) {
+	case Matrix:
+		B := in[0].(Matrix)
+		sA := A.Size()
+		sB := B.Size()
+		if (sA.Y != sB.Y) || (sA.X != sB.X) {
+			err = fmt.Errorf("go.iccp/matrix: Matrix dimensions mis-match.")
+			return
 		}
+
+		A.Foreach(func(y, x int) error {
+			A[y][x] += B[y][x]
+			return nil
+		})
+	case float64:
+		a := in[0].(float64)
+		A.Foreach(func(y, x int) error {
+			A[y][x] += a
+			return nil
+		})
+	case int:
+		a := float64(in[0].(int))
+		A.Foreach(func(y, x int) error {
+			A[y][x] += a
+			return nil
+		})
+	default:
+		err = fmt.Errorf("go.iccp/matrix: Invalid input for method Add.")
 	}
 	return
 }
--- matrix/operations_test.go
+++ matrix/operations_test.go
@@ -6,7 +6,22 @@ import (
 )
 
 func TestOperations(t *testing.T) {
-	A, _ := New("1,2,3;4,5,6")
-	B, _ := New("4,5,6;7,8,9")
-	C, _ := A.Add(B)
+	A, err := New("1,2,3;4,5,6")
+	B, err := New("4,5,6;7,8,9")
+	err = A.Add(B)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = A.Add(1.2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(A)
+
+	err = A.Add("1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(A)
 }
